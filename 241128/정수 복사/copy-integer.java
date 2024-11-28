8545def6b185d66485b07d2f93adc16bf790441c
xoals1105 <72680084+xoals1105@users.noreply.github.com>
2024-11-28 21:01:12 +0900

    Title: 정수 복사, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241128/정수 복사/copy-integer.java
+++ 241128/정수 복사/copy-integer.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         int a = 3, b = 4;
-        a = b;
+        b = a;
 
         System.out.print(a"");
         System.out.println(b);
