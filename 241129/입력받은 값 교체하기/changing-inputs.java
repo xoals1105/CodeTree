eca47a01f140caadc986884b9b8aed8cfbbba826
xoals1105 <72680084+xoals1105@users.noreply.github.com>
2024-11-29 11:57:40 +0900

    Title: 입력받은 값 교체하기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241129/입력받은 값 교체하기/changing-inputs.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, temp;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.print(a+" "+b);    
+    }
+}
