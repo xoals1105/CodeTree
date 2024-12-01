d5d9afc5b66e4ed2df1dbd635a6fc199e79e980f
xoals1105 <72680084+xoals1105@users.noreply.github.com>
2024-12-01 23:19:32 +0900

    Title: 합과 평균, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 241201/합과 평균/sum-and-avg.java
+++ 241201/합과 평균/sum-and-avg.java
@@ -8,7 +8,7 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        System.out.print((a+b) + " " + (double)(a+b) / 2);
+        System.out.print(a+b + " " + (double)(a+b) / 2);
 
     }
 }
