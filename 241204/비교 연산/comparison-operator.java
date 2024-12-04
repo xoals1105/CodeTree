a29987841357a22a5913b4a631b4c4561af55aab
xoals1105 <72680084+xoals1105@users.noreply.github.com>
2024-12-04 23:41:56 +0900

    Title: 비교 연산, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241204/비교 연산/comparison-operator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언
+        int a, b;
+        
+        // 입력
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println(a >= b ? 1 : 0);
+        System.out.println(a > b ? 1 : 0);
+        System.out.println(a <= b ? 1 : 0);
+        System.out.println(a < b ? 1 : 0);
+        System.out.println(a == b ? 1 : 0);
+        System.out.println(a != b ? 1 : 0);
+    }
+}
