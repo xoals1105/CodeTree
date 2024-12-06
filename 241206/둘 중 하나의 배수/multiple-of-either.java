25185e65576c319feb1bcaa34dfebfea0399806e
xoals1105 <72680084+xoals1105@users.noreply.github.com>
2024-12-06 17:15:16 +0900

    Title: 둘 중 하나의 배수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241206/둘 중 하나의 배수/multiple-of-either.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a;
+
+        a = sc.nextInt();
+
+        if (a % 3 == 0 || a % 5 ==)
+            System.out.println("1");
+        else    
+            System.out.println("o");
+    }
+}
