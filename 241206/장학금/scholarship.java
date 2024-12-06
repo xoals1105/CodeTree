923f217cdeea5a35a45c4e0fadc5a839b1ca23a6
xoals1105 <72680084+xoals1105@users.noreply.github.com>
2024-12-06 17:26:47 +0900

    Title: 장학금, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241206/장학금/scholarship.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        if (a < 90)
+            System.out.print("0");
+        else if (b < 95)
+            System.out.print("50000");
+        else    
+            System.out.print("100000");
+
+
+    }
+}
