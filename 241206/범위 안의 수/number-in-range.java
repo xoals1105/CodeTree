7bebbacd661f5a5519cfb83a062103f09c3dd844
xoals1105 <72680084+xoals1105@users.noreply.github.com>
2024-12-06 16:46:56 +0900

    Title: 범위 안의 수, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241206/범위 안의 수/number-in-range.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a;
+
+        a = sc.nextInt();
+
+        if (a >= 10 && a <= 20)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+        
+
+    }
+}
