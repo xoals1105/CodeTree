b1a2c043df9d7ac616325567233a767ac3f9f620
xoals1105 <72680084+xoals1105@users.noreply.github.com>
2024-12-06 18:39:27 +0900

    Title: 남녀노소 구분짓기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241206/남녀노소 구분짓기/sex-and-age.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int sex, age;
+
+        sex = sc.nextInt();
+        age = sc.nextInt();
+
+        if (sex == 0) {
+            if(age >= 19)
+                System.out.println("MAN");
+            else
+                System.out.println("BOY");
+        }
+
+        if (sex == 1) {
+            if(age >= 19)
+                System.out.println("WOMAN");
+            else
+                System.out.println("GIRL");
+        }
+
+
+    }
+}
