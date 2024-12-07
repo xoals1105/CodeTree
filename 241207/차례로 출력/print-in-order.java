22b3c56ae4f67d0badb4e7c697ce20807519e30b
xoals1105 <72680084+xoals1105@users.noreply.github.com>
2024-12-07 18:31:08 +0900

    Title: 차례로 출력, Time: 83ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241207/차례로 출력/print-in-order.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        for (int a = 5; a < 18; a++)
+            System.out.print(a + " ");
+    
+    }
+}
