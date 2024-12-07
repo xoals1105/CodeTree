111a6692099ecd1200b86a81956c4b26affc09e3
xoals1105 <72680084+xoals1105@users.noreply.github.com>
2024-12-07 15:46:54 +0900

    Title: 윤년인가, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241207/윤년인가/is-leap-year.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언
+        int y;
+        
+        // 입력
+        y = sc.nextInt();
+        
+        if((y % 4 == 0 && y % 100 != 0) || y % 400 == 0)
+            System.out.println("true");
+        else
+            System.out.println("false");
+    }
+}
