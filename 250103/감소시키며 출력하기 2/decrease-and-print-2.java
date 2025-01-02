db855c10dfeba73e64a58e5c646cd5727f81ebef
xoals1105 <72680084+xoals1105@users.noreply.github.com>
2025-01-03 07:10:25 +0900

    Title: 감소시키며 출력하기 2, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250103/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int n = 26;
+
+        while(n >= 10) {
+            System.out.print(n + " ");
+            n--;
+        }
+    }
+}
