64e8bb667cf693841ae6df1ed9ba680dcc9bd6af
xoals1105 <72680084+xoals1105@users.noreply.github.com>
2025-01-03 06:33:11 +0900

    Title: 증가시키며 출력하기 3, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250103/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        int a = 10;
+
+        while(a < 27) {
+            System.out.print(a+ " ");
+            a += 2;
+        }
+    }
+}
