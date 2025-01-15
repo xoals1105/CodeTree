9346a735cd5eeee7825b10eb0997c0b545d2dd31
xoals1105 <72680084+xoals1105@users.noreply.github.com>
2025-01-15 22:24:36 +0900

    Title: 높은 수에서 낮은 수까지, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250115/높은 수에서 낮은 수까지/from-high-to-low.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        if(a > b) {
+            for(int i = a; i >= b; i--)
+                System.out.print(i + " ");
+        }
+
+        else {
+            for(int i = b; i >= a; i--)
+                System.out.print(i + " ");
+        }
+    }
+}
