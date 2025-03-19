9e71684f9b1cfecc146d094914111084b87fee04
xoals1105 <72680084+xoals1105@users.noreply.github.com>
2025-03-19 13:27:57 +0900

    Title: 홀수만 출력하기, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

--- 250319/홀수만 출력하기/print-only-odd-numbers.java
+++ 250319/홀수만 출력하기/print-only-odd-numbers.java
@@ -4,27 +4,17 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
-
-        int a,b,c,d,e,f;
-
-        a = sc.nextInt();
-        b = sc.nextInt();
-        c = sc.nextInt();
-        d = sc.nextInt();
-        e = sc.nextInt();
-        f = sc.nextInt();
-
-        if(a % 2 == 1 && a % 3 == 0)
-            System.out.println(a);
-        if(b % 2 == 1 && b % 3 == 0)
-            System.out.println(b);
-        if(c % 2 == 1 && c % 3 == 0)
-            System.out.println(c);
-        if(d % 2 == 1 && d % 3 == 0)
-            System.out.println(d);
-        if(e % 2 == 1 && e % 3 == 0)
-            System.out.println(e);
-        if(f % 2 == 1 && f % 3 == 0)
-            System.out.println(f);
+    // Read the number of integers N
+        int n = sc.nextInt();
+        
+        // Loop through the next N integers
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+            
+            // Check if the number is odd
+            if (num % 2 != 0 && num % 3 == 0) {
+                System.out.println(num);
+            }
+        }
     }
 }
