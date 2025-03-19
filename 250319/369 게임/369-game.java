1c44eee9ccb32aad91785ec85091488c6fb8ce38
xoals1105 <72680084+xoals1105@users.noreply.github.com>
2025-03-19 11:53:04 +0900

    Title: 369 게임, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 250319/369 게임/369-game.java
+++ 250319/369 게임/369-game.java
@@ -16,6 +16,8 @@ public class Main {
                 System.out.print("0 ");
             else if(i % 10 == 3 || i % 10 == 6 || i % 10 == 9)
                 System.out.print("0 ");
+            else if(i / 10 == 3 || i / 10 == 6 || i / 10 == 9)
+                System.out.print("0 ");
             else
                 System.out.print(i + " ");
         }
