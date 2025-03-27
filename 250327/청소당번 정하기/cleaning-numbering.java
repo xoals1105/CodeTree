a36d78db92016373ac6064dcc172e4e1077c6299
xoals1105 <72680084+xoals1105@users.noreply.github.com>
2025-03-27 17:48:07 +0900

    Title: 청소당번 정하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250327/청소당번 정하기/cleaning-numbering.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 변수 선언 및 입력
+        int n;
+        n = sc.nextInt();
+
+        int cnt2 = 0, cnt3 = 0, cnt12 = 0;
+    
+        // 각 날짜마다 확인합니다.
+        for(int i = 1; i <= n; i++) {
+            // 주기가 가장 긴 12일부터 확인합니다.
+            if(i % 12 == 0)
+                cnt12++;
+            // 12일 주기에 들어오지 않는다면, 3일 주기에 들어오는지 확인합니다.
+            else if(i % 3 == 0)
+            오는지 확인합니다.
+            else if(i % 2 == 0)    cnt3++;
+            // 3일 주기에도 들어오지 않는다면, 2일 주기에 들어
+                cnt2++;
+        }
+
+        System.out.println(cnt2 + " " + cnt3 + " " + cnt12);
+    }
+}
