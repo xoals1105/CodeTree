3c8486465dc473ee4866ddd47666f3b3670c2727
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-10 17:06:28 +0900

    Title: ab 사이에 있는 c, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250810/ab 사이에 있는 c/c-between-a-and-b.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean finder = false;
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        for (int i = a; i <= b; i++)
+        {
+            if( i % c == 0)
+            {
+                finder = true;
+                break;
+            }
+        }
+
+        if (finder == true) System.out.println("Yes");
+
+        else System.out.println("No");
+      
+
+    }
+}
