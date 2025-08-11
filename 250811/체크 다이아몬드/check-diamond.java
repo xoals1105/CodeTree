b3ab81b694c0e82443e6ccdd0a76eed652d89156
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-11 16:58:41 +0900

    Title: 체크 다이아몬드, Time: 136ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250811/체크 다이아몬드/check-diamond.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < (n - i - 1); j++)
+            {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 2; i <= n; i++)
+        {
+            for (int j = 0; j < (i - 1); j++)
+            {
+                System.out.print(" ");
+            }
+            
+            for (int j = 0; j < (n - i + 1); j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        } 
+    }
+}
