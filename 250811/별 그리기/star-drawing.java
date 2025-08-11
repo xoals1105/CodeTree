e90c655a0b989e10c8f13f1e70f11a59a1d9ce27
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-11 16:29:06 +0900

    Title: 별 그리기, Time: 175ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250811/별 그리기/star-drawing.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < (n - 1 - i); j++)
+            {
+                System.out.print(" ");
+            }
+            
+            for (int j = 0; j <= (i * 2); j++)
+            {
+                System.out.print("*");
+            }    
+
+            System.out.println();
+        }
+
+        for (int i = (n - 1); i > 0; i--)
+        {
+            for (int j = 0; j < (n - i); j++)
+            {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < (i * 2) - 1; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
