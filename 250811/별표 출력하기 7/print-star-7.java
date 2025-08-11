9ac7bc1646d8e855718dde3df81c4c0cf64b0e5e
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-11 11:26:25 +0900

    Title: 별표 출력하기 7, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

--- 250811/별표 출력하기 7/print-star-7.java
+++ 250811/별표 출력하기 7/print-star-7.java
@@ -9,7 +9,7 @@ public class Main {
 
         for (int i = 0; i < n; i++)
         {
-            for (int j = 0; j < i; j++)
+            for (int j = 0; j <= i; j++)
             {
                 System.out.print("* ");
             }
