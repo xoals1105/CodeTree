475410df12450dfb75ca1f2f41fa3e732eebd9b2
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-12 10:34:03 +0900

    Title: 9-1 정사각형, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

--- 250812/9-1 정사각형/9-1-square.java
+++ 250812/9-1 정사각형/9-1-square.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     
-        Scanner scanner = new Sacnner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
         int cnt = 9;
