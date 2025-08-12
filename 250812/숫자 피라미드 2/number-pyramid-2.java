4517ca1f6b0e0493abbc1f7b4f4660847a842d4a
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-12 12:16:59 +0900

    Title: 숫자 피라미드 2, Time: 179ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250812/숫자 피라미드 2/number-pyramid-2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int cnt = 1;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < i + 1; j++)
+            {
+                System.out.print(cnt + " ");
+                cnt++;
+            }
+            System.out.println();
+        }
+    }
+}
