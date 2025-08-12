4724cf1cc2409684bb8dda51f9cd3d0a464bb71d
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-12 15:42:55 +0900

    Title: 알파벳 출력하기, Time: 134ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250812/알파벳 출력하기/print-alphabet.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        char alphabet = 'A';
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < i + 1; j++)
+            {
+                System.out.print(alphabet);
+                alphabet++;
+
+                if (alphabet > 'Z')
+                    alphabet = 'A';
+            }
+            System.out.println();
+        }
+    }
+}
