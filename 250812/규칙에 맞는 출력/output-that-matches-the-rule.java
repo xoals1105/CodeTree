1ff2e890836e9243cb9866864b95706e5d0c44f4
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-12 13:50:52 +0900

    Title: 규칙에 맞는 출력, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250812/규칙에 맞는 출력/output-that-matches-the-rule.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        
+        for (int i = n; i > 0; i--)
+        {
+            for (int j = i; j <= n; j++)
+            {
+                System.out.print(j  + " ");
+                              
+            }
+  
+            System.out.println();
+        }
+    }
+}
