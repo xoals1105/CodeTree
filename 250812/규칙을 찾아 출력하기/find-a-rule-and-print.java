ee80bf62f645ed99d756b42066950f7f2b878861
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-12 09:33:07 +0900

    Title: 규칙을 찾아 출력하기, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250812/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i == 0 || i == (n - 1) || j == 0 || j == (n - 1))
+                    System.out.print("* ");
+                else if (j + 1 <= i)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+
+            System.out.println();
+        }
+    }
+}
