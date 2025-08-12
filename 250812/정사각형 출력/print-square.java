b7cb9eb2a72d5ac62814e403d9becbefaeb7c1af
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-12 10:23:40 +0900

    Title: 정사각형 출력, Time: 154ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250812/정사각형 출력/print-square.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int cnt = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                System.out.print(cnt + " ");
+                cnt++;
+            }
+            System.out.println();
+            
+        }
+    }
+}
