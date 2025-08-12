c5b51577edd9091caa3e932bd06982f1e97fe3f2
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-12 12:12:59 +0900

    Title: 숫자 피라미드 3, Time: 144ms, Memory: 10MB, Status: Passed - Codetree

--- 250812/숫자 피라미드 3/number-pyramid-3.java
+++ 250812/숫자 피라미드 3/number-pyramid-3.java
@@ -10,7 +10,7 @@ public class Main {
         for (int i = 1; i <= n; i++)
         {
             for (int j = 1; j <= i; j++)
-                System.out.print(i * j);
+                System.out.print(i * j + " ");
             System.out.println();
         }
     }
