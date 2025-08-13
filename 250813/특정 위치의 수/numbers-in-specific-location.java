4865cca57b1d38a73cfb82640864634c9e39c997
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-13 11:22:01 +0900

    Title: 특정 위치의 수, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250813/특정 위치의 수/numbers-in-specific-location.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = new int[10];
+       
+
+        for(int i = 0; i < 10; i++)
+        {
+            arr[i] = scanner.nextInt();
+        }
+
+   
+
+        System.out.println(arr[2] + arr[4] + arr[9]);
+
+    }
+}
