84b13f5b4f3dbc2716cca2f381e3470e747ef5ab
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-13 22:15:31 +0900

    Title: 연속부분수열일까, Time: 137ms, Memory: 10MB, Status: Passed - Codetree

--- 250813/연속부분수열일까/contiguous-array-or-not.java
+++ 250813/연속부분수열일까/contiguous-array-or-not.java
@@ -2,47 +2,41 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    
+
         Scanner scanner = new Scanner(System.in);
 
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
+        int a = scanner.nextInt(); // N1
+        int b = scanner.nextInt(); // N2
 
         int[] a_arr = new int[a];
         int[] b_arr = new int[b];
 
-        for(int i = 0; i < a; i++)
-        {
+        for (int i = 0; i < a; i++) {
             a_arr[i] = scanner.nextInt();
         }
 
-        for(int i = 0; i < b; i++)
-        {
+        for (int i = 0; i < b; i++) {
             b_arr[i] = scanner.nextInt();
         }
 
         boolean found = false;
 
-        for(int i = 0; i < a; i++)
-        {
-            int cnt = 0;
-
-            for(int j = 0; j < b; j++)
-            {
-                if(a_arr[i + j] == b_arr[j])  
-                    cnt++;                
-                else
-                    break;
-
-    
+        // A에서 시작 위치를 하나씩 잡고 확인
+        for (int start = 0; start <= a - b; start++) {
+            int cnt = 0; // 여기서 cnt는 "연속으로 맞은 개수"
+            for (int j = 0; j < b; j++) {
+                if (a_arr[start + j] == b_arr[j]) {
+                    cnt++;
+                } else {
+                    break; // 하나라도 틀리면 그 시작 위치는 실패
+                }
             }
-
-            if(cnt == b) 
-            {
+            if (cnt == b) {
                 found = true;
                 break;
             }
         }
+
         System.out.println(found ? "Yes" : "No");
     }
 }
