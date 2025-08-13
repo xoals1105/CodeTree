dd6ff8f527846087aac4904ef6c44cadf56c403a
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-13 11:09:24 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 137ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250813/시험 통과 여부 확인하기 2/verify-test-passed-2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        int person = scanner.nextInt();
+        int pass_count = 0;
+        for (int i = 0; i < person; i++)
+        {
+            int[] arr = new int[4];
+            int sum = 0;
+            int cnt = 0;
+
+            for(int j = 0; j < 4; j++)
+            {
+                arr[i] = scanner.nextInt();
+                sum += arr[i];
+                cnt++;
+            }
+            
+            int avg = sum / cnt;
+            
+            if(avg >= 60)
+            { 
+                System.out.println("pass");
+                pass_count++;
+            }
+            else System.out.println("fail");
+        }
+
+        System.out.println(pass_count);
+    }
+}
