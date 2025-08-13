e29a428fdfe3e6cd325e5fa6a28979ec3669734e
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-13 16:40:52 +0900

    Title: 배열 놀이, Time: 166ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250813/배열 놀이/play-with-array.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int q = scanner.nextInt();
+
+        int[] elementArr = new int[n];
+        int[] one_questionArr = new int[q];
+        int[] two_questionArr = new int[q];
+        int[] three_questionArr = new int[q];
+
+        int index = -1;
+
+        for(int i = 0; i < n; i++)
+        {
+            elementArr[i] = scanner.nextInt();
+        }
+
+        for(int i = 0; i < q; i++)
+        {
+            one_questionArr[i] = scanner.nextInt();
+            two_questionArr[i] = scanner.nextInt();
+            
+            switch(one_questionArr[i])
+            {
+                case 1:
+                    System.out.println(elementArr[two_questionArr[i] - 1]);
+                    break;
+
+                case 2:
+                    for(int j = 0; j < n; j++)
+                    {
+                        if(elementArr[j] == two_questionArr[i]) 
+                        {
+                            index = j + 1;
+                            break;
+                        }
+                    }
+
+                    if(index == -1) System.out.println("0");
+                    else System.out.println(index);
+                    break;
+                    
+                case 3:
+                    three_questionArr[i] = scanner.nextInt();
+
+                    for(int j = two_questionArr[i]; j <= three_questionArr[i]; j++)
+                    {
+                        System.out.print(elementArr[j - 1] + " ");
+                    }
+                    System.out.println();
+                    break;
+            }
+        }
+    }
+}
