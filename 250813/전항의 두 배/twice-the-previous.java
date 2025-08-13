c8fcb1caa4602ecc6c68d69aa4e878ffa27833ae
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-13 14:56:12 +0900

    Title: 전항의 두 배, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250813/전항의 두 배/twice-the-previous.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = new int[10];
+        arr[0] = scanner.nextInt();
+        arr[1] = scanner.nextInt();
+
+        for(int i = 2; i < 10; i++)
+            arr[i] = arr[i - 1] + (2 * arr[i - 2]);
+        
+
+        for(int i = 0; i < 10; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
