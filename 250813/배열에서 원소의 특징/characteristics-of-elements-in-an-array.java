17aa5e5fdc34c70ba696fd3e7b70219190a89ea9
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-13 12:06:08 +0900

    Title: 배열에서 원소의 특징, Time: 125ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250813/배열에서 원소의 특징/characteristics-of-elements-in-an-array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = new int[10];
+        int num = 0;
+        for(int i = 0; i < 10; i++)
+        {
+            arr[i] = scanner.nextInt();
+
+            if(arr[i] == 3)
+            {
+                num = i;
+                break;
+            }
+        }
+        System.out.print(arr[num -1]);
+    }
+}
