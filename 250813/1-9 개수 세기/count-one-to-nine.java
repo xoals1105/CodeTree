e38b12e183c807f2304ac8ae526194b266786e35
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-13 15:06:43 +0900

    Title: 1-9 개수 세기, Time: 140ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250813/1-9 개수 세기/count-one-to-nine.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        int[] countArr = new int[10];
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = scanner.nextInt();
+            countArr[arr[i]]++;
+
+        }
+
+
+        for(int i = 1; i <= 9; i++)
+        {
+            System.out.println(countArr[i]);
+        }
+
+    }
+}
