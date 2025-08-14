7b9fce600ffa65923ca954fc465a14c85c5b2d1f
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-14 11:54:40 +0900

    Title: 배열의 평균, Time: 128ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250814/배열의 평균/ave-of-array.java
+++ 250814/배열의 평균/ave-of-array.java
@@ -24,8 +24,8 @@ public class Main {
                 sum += arr[i][j];
                 sum2 += arr[i][j]; 
             }
-            double avg = (double) (sum / 4);
-            System.out.print(avg + " ");
+            int avg = (sum / 4);
+            System.out.print(avg + ".0 ");
         }
         System.out.println();
 
@@ -36,12 +36,12 @@ public class Main {
             {
                 sum1 += arr[j][i];
             }
-            double avg = (double) (sum1 / 2);
-            System.out.print(avg + " ");
+            int avg = (sum1 / 2);
+            System.out.print(avg + ".0 ");
         }
         System.out.println();
 
-        double avg = (double) (sum2 / 8);
-        System.out.print(avg + " ");
+        int avg = (sum2 / 8);
+        System.out.print(avg + ".0 ");
     }
 }
