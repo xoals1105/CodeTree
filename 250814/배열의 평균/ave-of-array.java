cf9aecf8bce71001fadf01a8f4b72b4f39575a9a
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-14 11:53:51 +0900

    Title: 배열의 평균, Time: 135ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250814/배열의 평균/ave-of-array.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        int[][] arr = new int[2][4];
+        int sum2 = 0;
+
+        for(int i = 0; i < 2; i++)
+        {
+            for(int j = 0; j < 4; j++)
+            {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+
+        for(int i = 0; i < 2; i++)
+        {
+            int sum = 0;
+            for(int j = 0; j < 4; j++)
+            {
+                sum += arr[i][j];
+                sum2 += arr[i][j]; 
+            }
+            double avg = (double) (sum / 4);
+            System.out.print(avg + " ");
+        }
+        System.out.println();
+
+        for(int i = 0; i < 4; i++)
+        {
+            int sum1 = 0;
+            for(int j = 0; j < 2; j++)
+            {
+                sum1 += arr[j][i];
+            }
+            double avg = (double) (sum1 / 2);
+            System.out.print(avg + " ");
+        }
+        System.out.println();
+
+        double avg = (double) (sum2 / 8);
+        System.out.print(avg + " ");
+    }
+}
