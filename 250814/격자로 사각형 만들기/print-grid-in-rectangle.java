17afcd31c1ab414affb724d33691bd91472d7f59
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-14 15:04:49 +0900

    Title: 격자로 사각형 만들기, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250814/격자로 사각형 만들기/print-grid-in-rectangle.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[][] arr = new int[n][n];
+
+        for(int i = 0; i < n; i++)
+        {
+            arr[0][i] = 1;
+            arr[i][0] = 1;
+        }
+
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = 1; j < n; j++)
+            {
+                arr[i][j] = arr[i - 1][j] + arr[i][j - 1] + arr[i - 1][j - 1]; 
+            }
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+                System.out.print(arr[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
