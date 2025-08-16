27e82022f40099cf5c680a33e012fc045e22e367
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-16 20:26:47 +0900

    Title: 문자열 범위 출력하기, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250816/문자열 범위 출력하기/print-string-in-range.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        String n = scanner.nextLine();
+
+        for(int i = 2; i < 10; i++)
+            System.out.print(n.charAt(i));
+    }
+}
