45febf72d3cd58336103ccfe430d9526c0da6673
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-18 13:23:09 +0900

    Title: 문자열 범위 출력하기 2, Time: 112ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250818/문자열 범위 출력하기 2/print-string-in-range-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        String string = scanner.next();
+        int num = scanner.nextInt();
+
+        for(int i = string.length() - 1; i >= string.length() - num; i--)
+            System.out.print(string.charAt(i));
+    }
+}
