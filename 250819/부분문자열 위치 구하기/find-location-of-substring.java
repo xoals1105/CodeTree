0928193686883d5f99b3e8dff961d2e431c64d6c
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-19 15:38:54 +0900

    Title: 부분문자열 위치 구하기, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

--- 250819/부분문자열 위치 구하기/find-location-of-substring.java
+++ 250819/부분문자열 위치 구하기/find-location-of-substring.java
@@ -9,11 +9,12 @@ public class Main {
         String found_string = scanner.next();
 
         int length = string.length();
+        int found_length = found_string.length();
         int n = -1;
 
-        for(int i = 0; i < length - 1; i++)
+        for(int i = 0; i < length - found_length + 1; i++)
         {
-            if(string.substring(i, i + 2).equals(found_string))
+            if(string.substring(i, i + found_length).equals(found_string))
             {
                 n = i;
                 break;
