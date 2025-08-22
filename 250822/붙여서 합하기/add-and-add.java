5ce75059a1853e10e74156e585c55e15a3d1e79d
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-22 12:56:23 +0900

    Title: 붙여서 합하기, Time: 117ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250822/붙여서 합하기/add-and-add.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        String a = scanner.next();
+        String b = scanner.next();
+
+        System.out.println(Integer.parseInt(a + b) + Integer.parseInt(b + a));
+    }
+}
