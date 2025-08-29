2e96edfd7ee2b9318c0779e495ca4efa3c4ec8db
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-29 13:32:04 +0900

    Title: 함수를 이용한 369 게임, Time: 140ms, Memory: 10MB, Status: Passed - Codetree

--- 250829/함수를 이용한 369 게임/369-games-using-functions.java
+++ 250829/함수를 이용한 369 게임/369-games-using-functions.java
@@ -1,9 +1,21 @@
 import java.util.Scanner;
 public class Main {
 
+    public static boolean contains369(int num)
+    {
+        while(num > 0)
+        {
+            if(num % 10 == 3 || num % 10 == 6 || num % 10 == 9)
+                return true;
+            num /= 10;
+        }
+
+        return false;
+    }
+
     public static boolean isMagicNumber(int num)
     {
-        return (num / 10 == 3) || (num % 10 == 3) || (num / 10 == 6) || (num % 10 == 6) || (num / 10 == 9) || (num % 10 == 9) || num % 3 == 0;
+        return contains369(num) || num % 3 == 0;
     }
 
     public static void main(String[] args) {
