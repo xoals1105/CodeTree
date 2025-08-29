aaf16af25b7597899f4ee0edc02d8e0b9819570d
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-08-29 14:38:17 +0900

    Title: 함수를 이용한 온전수 판별, Time: 119ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250829/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Main {
+
+    public static boolean isSatisfaction(int num)
+    {
+        if(num % 2 == 0)
+            return false;
+
+        if(num % 10 == 5)
+            return false;
+
+        if(num % 3 == 0 && num % 9 != 0)
+            return false;
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int cnt = 0;
+
+        for(int i = a; i <= b; i++)
+        {
+            if(isSatisfaction(i)) cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
