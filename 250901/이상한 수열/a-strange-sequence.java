47794ee5026fe57d247541f43b8b723b525bb743
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-01 19:33:27 +0900

    Title: 이상한 수열, Time: 154ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250901/이상한 수열/a-strange-sequence.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+
+    public static int Calculation(int n)
+    {
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        return Calculation(n / 3) + Calculation(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    
+        System.out.println(Calculation(n));
+    }
+}
