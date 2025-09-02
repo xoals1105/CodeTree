e05032d1a5118f7d7d69fa69cb70846aa76ab71e
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-02 11:25:00 +0900

    Title: k번째로 신기한 문자열, Time: 141ms, Memory: 10MB, Status: Passed - Codetree

--- 250902/k번째로 신기한 문자열/kth-special-string.java
+++ 250902/k번째로 신기한 문자열/kth-special-string.java
@@ -2,36 +2,46 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
-
-    public static String FindString(String[] words, int k, String t)
-    {
-        int cnt = 0;
-        String[] tStartingString = new String[words.length];
-
-        for(int i = 0; i < words.length; i++)
-        {
-            if(words[i].substring(0, t.length()).equals(t))
-            {
-                tStartingString[cnt] = words[i];
-                cnt++;
-            }   
-        }
-
-        Arrays.sort(tStartingString, 0, cnt);
-
-        return tStartingString[k - 1];
+    public static final int MAX_N = 100;
+    
+    public static int n, k;
+    public static String t;
+    public static int cnt;
+    
+    public static String[] str = new String[MAX_N];
+    public static String[] words = new String[MAX_N];
+    
+    // a가 b로 시작하는지를 확인합니다.
+    public static boolean startsWith(String a, String b) {
+        // b의 길이가 더 길 수는 없습니다.
+ 
+        // b의 길이만큼 살펴보며, a와 문자열이 완벽히 동일한지 확인합니다.
+        for(int i = 0; i < (int) b.length(); i++)
+            if(a.charAt(i) != b.charAt(i))
+                return false;
+        
+        // 전부 확인했는데도 오류가 없다면, prefix라 할 수 있습니다.
+        return true;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        String t = sc.next();
-        String[] words = new String[n];
-        for (int i = 0; i < n; i++) {
-            words[i] = sc.next();
+        // 변수 선언
+        n = sc.nextInt();
+        k = sc.nextInt();
+        t = sc.next();
+        for(int i = 0; i < n; i++) {
+            str[i] = sc.next();
+            
+            // 문자열 t로 시작하는 경우인지 확인하고,
+            // 그 경우에만 목록에 넣어줍니다.
+            if(startsWith(str[i], t))
+                words[cnt++] = str[i];
         }
         
-        System.out.println(FindString(words, k, t));
+        // 정렬을 진행합니다.
+        Arrays.sort(words, 0, cnt);
+        
+        System.out.print(words[k - 1]);
     }
 }
