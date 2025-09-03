893b6a5f782837f73cfc92f080710191a5b4dee7
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-03 11:48:35 +0900

    Title: 비오는 날, Time: 139ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250903/비오는 날/rainy-day.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class WeatherReport
+{
+    String date = "";
+    String day = "";
+    String weather = "";
+
+    public WeatherReport(String date, String day, String weather)
+    {
+        this.date = date;
+        this.day = day;
+        this.weather = weather;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        WeatherReport[] weatherReport = new WeatherReport[n];
+        for (int i = 0; i < n; i++) {
+            String date = sc.next();
+            String day = sc.next();
+            String weather = sc.next();
+        
+            weatherReport[i] = new WeatherReport(date, day, weather);
+        }
+
+        int firstIdx = 0;
+        for(int i = 1; i < n; i++)
+        {
+            if(weatherReport[firstIdx].day.compareTo(weatherReport[i].day) < 0) firstIdx = i;
+        }
+
+        System.out.println(weatherReport[firstIdx].date + " " + weatherReport[firstIdx].day + " " + weatherReport[firstIdx].weather);
+    }
+}
