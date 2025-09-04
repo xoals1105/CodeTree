0527525ec6523eb85a8b86a5a63d1a577d50a73a
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-04 17:03:09 +0900

    Title: 최대로 겹치는 구간, Time: 143ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250904/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 250904/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] section = new int[100];
+        int[] section = new int[300];
         int[] section_second = new int[100];
         int[] section_three = new int[100];
 
@@ -11,13 +11,19 @@ public class Main {
             int x1 = sc.nextInt();
             int x2 = sc.nextInt();
 
+            x1 += 100;
+            x2 += 100;
+
             for(int j = x1; j <= x2; j++)
                 section[j] += 1; 
         }
 
-        int line = 0;
-        for(int i = 0; i < section.length; i++)
-            if(section[i] > 1) line++;
-        System.out.println(line);
+        int maxOverlap = 0;
+        for(int i = 0; i < section.length; i++) {
+            if(section[i] > maxOverlap)
+                maxOverlap = section[i];
+        }
+
+        System.out.println(maxOverlap);
     }
 }
