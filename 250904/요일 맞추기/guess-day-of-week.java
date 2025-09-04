4227e1a2511e7f14bf871935f6f34205f016607a
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-04 11:22:39 +0900

    Title: 요일 맞추기, Time: 134ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250904/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+class InitialDay
+{
+    int month = 0;
+    int day = 0;
+    String dayOfWeek = "Mon";
+
+    public InitialDay(int month, int day)
+    {
+        this.month = month;
+        this.day = day;
+    }
+}
+
+public class Main 
+{
+    public static String FindDay(int month, int day, int m, int d)
+    {
+        String[] dayOfWeek = new String[] {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        int[] dayOfMonth = new int[] {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        if(month < m || (month == m && day < d))
+        {
+            int isDayOfWeek = 0;
+            while(true)
+            {
+                if(month == m && day == d) break;
+
+                if(dayOfWeek[isDayOfWeek].equals("Sun")) isDayOfWeek = -1;
+
+                day++;
+                isDayOfWeek++;
+
+                if(day > dayOfMonth[month])
+                {
+                    month++;
+                    day = 0;
+                }
+            }
+
+            return dayOfWeek[isDayOfWeek];
+        }
+
+        else
+        {
+            int isDayOfWeek = 0;
+            while(true)
+            {
+                if(month == m && day == d) break;
+
+                if(dayOfWeek[isDayOfWeek].equals("Mon")) isDayOfWeek = 7;
+
+                day--;
+                isDayOfWeek--;
+
+                if(day < 0)
+                {
+                    month--;
+                    day = dayOfMonth[month];
+                }
+            }
+
+            return dayOfWeek[isDayOfWeek];
+        }
+        
+    }
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+    
+        InitialDay initialday = new InitialDay(m1, d1);
+
+        System.out.println(FindDay(initialday.month, initialday.day, m2, d2));
+    }
+}
