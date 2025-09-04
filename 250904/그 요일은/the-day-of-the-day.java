b3346a5f53de95597e90eb8f8b1f8c7171e9fa8d
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-04 11:51:44 +0900

    Title: 그 요일은, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 250904/그 요일은/the-day-of-the-day.java
+++ 250904/그 요일은/the-day-of-the-day.java
@@ -25,10 +25,10 @@ public class Main {
 
         String[] dayOfWeek = new String[] {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
         int cnt = 0;
-        int day = 0;
+        int day = 1;
         for(int i = 0; i < diff; i++)
         {
-            if(day == 6) day = 0;
+            if(day == 7) day = 0;
             if(A.equals(dayOfWeek[day])) cnt++;
             day++;
         }
