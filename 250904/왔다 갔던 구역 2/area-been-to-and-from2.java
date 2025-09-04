46ac69dca1238cb34483245bec1e635892f10aa6
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-04 18:44:12 +0900

    Title: 왔다 갔던 구역 2, Time: 138ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250904/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 250904/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -4,28 +4,35 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        int[] digits = new int[600];
-        for (int i = 0; i < N; i++) {
-            int x = sc.nextInt();
-            char dir = sc.next().charAt(0);
-            int initialDistance = 300;
-
-            for(int j = 0; j < x; j++)
-            {
-            switch(dir)
-            {
-                case 'L':  
-                    digits[initialDistance] += 1;
-                    initialDistance++;
-                    break;
-                case 'R':
-                    digits[initialDistance] += 1;  
-                    initialDistance--;
-                    break;
-            }
-            }
+        int[] x = new int[N];
+        int[] dir = new int[N];
         
+        int[] digits = new int[N*10];
+        for (int i = 0; i < N; i++) 
+        {
+            x[i] = sc.nextInt();
+            dir[i] = sc.next().charAt(0);
         }
+
+        int initialDistance = (N*10) / 2;
+        for(int i = 0; i < N; i++)
+        {
+            for(int j = 0; j < x[i]; j++)
+            {
+                switch(dir[i])
+                {
+                    case 'L':  
+                        digits[initialDistance] += 1;
+                        initialDistance++;
+                        break;
+                    case 'R':
+                        digits[initialDistance] += 1;  
+                        initialDistance--;
+                        break;
+                }
+            }
+        }
+        
 
         int cnt = 0;
         for(int i = 0; i < digits.length; i++)
@@ -33,6 +40,6 @@ public class Main {
             if(digits[i] > 1) cnt++;
         }
 
-        System.out.println(cnt - 1);
+        System.out.println(cnt);
     }
 }
