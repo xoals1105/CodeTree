607936fecaa0da0570a46e00c0d20e542907acce
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-05 17:30:20 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 199ms, Memory: 27MB, Status: Passed - Codetree

--- 250905/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
+++ 250905/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
@@ -1,60 +1,53 @@
 import java.util.Scanner;
+
 public class Main {
-
-    public static int max = 1000;
-    public static int[][] rectangle = new int[2 * max + 1][2 * max + 1];
-    public static int num = 1;
-
-    public static void Coloring(int x1, int y1, int x2, int y2)
-    {   
-        for(int i = x1; i < x2; i++)
-            for(int j = y1; j < y2; j++)
-                rectangle[i][j] = num;
-        num++;
-    }
+    private static final int N = 3;          // 직사각형 개수
+    private static final int MAX_R = 2000;   // 격자 크기
+    private static final int OFFSET = 1000;  // 좌표 보정값 (상수)
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int ax1 = sc.nextInt();
-        int ay1 = sc.nextInt();
-        int ax2 = sc.nextInt();
-        int ay2 = sc.nextInt();
-        int bx1 = sc.nextInt();
-        int by1 = sc.nextInt();
-        int bx2 = sc.nextInt();
-        int by2 = sc.nextInt();
-        int mx1 = sc.nextInt();
-        int my1 = sc.nextInt();
-        int mx2 = sc.nextInt();
-        int my2 = sc.nextInt();
 
-        ax1 += max;
-        ay1 += max;
-        ax2 += max;
-        ay2 += max;
-        bx1 += max;
-        by1 += max;
-        bx2 += max;
-        by2 += max;
-        mx1 += max;
-        my1 += max;
-        mx2 += max;
-        my2 += max;
+        int[] x1 = new int[N];
+        int[] y1 = new int[N];
+        int[] x2 = new int[N];
+        int[] y2 = new int[N];
 
-        Coloring(ax1, ay1, ax2, ay2);
-        Coloring(bx1, by1, bx2, by2);
-        Coloring(mx1, my1, mx2, my2);
+        // 입력 처리
+        for (int i = 0; i < N; i++) {
+            x1[i] = sc.nextInt() + OFFSET;
+            y1[i] = sc.nextInt() + OFFSET;
+            x2[i] = sc.nextInt() + OFFSET;
+            y2[i] = sc.nextInt() + OFFSET;
+        }
 
-        int cnt = 0;
-        for(int i = 0; i < rectangle.length; i++)
-        {
-            for(int j = 0; j < rectangle[i].length; j++)
-            {
-                if(rectangle[i][j] == 1) cnt++;
-                if(rectangle[i][j] == 2) cnt++;
+        int result = calculateArea(x1, y1, x2, y2);
+        System.out.println(result);
+    }
+
+    // 면적 계산 메서드
+    private static int calculateArea(int[] x1, int[] y1, int[] x2, int[] y2) {
+        int[][] checked = new int[MAX_R + 1][MAX_R + 1];
+
+        // 직사각형에 1, 2, 3 번호 붙여 체크
+        for (int i = 0; i < N; i++) {
+            for (int x = x1[i]; x < x2[i]; x++) {
+                for (int y = y1[i]; y < y2[i]; y++) {
+                    checked[x][y] = i + 1;
+                }
             }
         }
 
-        System.out.println(cnt);
+        // A, B만 남아있는 영역(= 1, 2) 넓이 계산
+        int area = 0;
+        for (int x = 0; x <= MAX_R; x++) {
+            for (int y = 0; y <= MAX_R; y++) {
+                if (checked[x][y] == 1 || checked[x][y] == 2) {
+                    area++;
+                }
+            }
+        }
+
+        return area;
     }
 }
