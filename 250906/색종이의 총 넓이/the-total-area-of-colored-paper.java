b81f25d26a7831f64a8996981d29a512962c8a4e
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-06 12:48:28 +0900

    Title: 색종이의 총 넓이, Time: 143ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250906/색종이의 총 넓이/the-total-area-of-colored-paper.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Main {
+
+    public static final int maxRange = 300;
+    public static final int paperLength = 8;
+    public static final int offSet = 100; 
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] x = new int[n];
+        int[] y = new int[n];
+        
+        for (int i = 0; i < n; i++) 
+        {
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
+
+            x[i] += offSet;
+            y[i] += offSet;
+        }
+
+        int area = calculateRange(n, x, y);
+        System.out.println(area);
+    }
+
+    private static int calculateRange(int n, int[] x, int[] y)
+    {
+        int[][] checked = new int[maxRange + 1][maxRange + 1];
+
+        for(int i = 0; i < n; i++)
+            for(int j = x[i]; j < x[i] + paperLength; j++)
+                for(int z = y[i]; z < y[i] + paperLength; z++)
+                    checked[j][z] = 1;
+
+        int cnt = 0;
+        for(int i = 0; i < maxRange; i++)
+            for(int j = 0; j < maxRange; j++)
+                if(checked[i][j] == 1) cnt++;            
+
+        return cnt;
+    }
+}
