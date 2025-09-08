a529dcd27a56709a981ce0f69960b72316ade7b5
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-08 14:10:37 +0900

    Title: 되돌아오기 2, Time: 148ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250908/되돌아오기 2/come-back-2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Main {
+
+    public static int[] dx = new int[] {1, 0, -1, 0};
+    public static int[] dy = new int[] {0, -1, 0, 1};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String commands = sc.next();
+
+        int direction = 3;
+        int x = 0;
+        int y = 0;
+        int cnt = 0;
+        for(int i = 0; i < commands.length(); i++)
+        {
+            switch(commands.charAt(i))
+            {
+                case 'L':
+                    direction = (direction + 3) % 4;            
+                    break;
+                case 'R':
+                    direction = (direction + 1) % 4;            
+                    break;
+                case 'F':
+                    x += dx[direction];
+                    y += dy[direction];
+                    break;
+            }
+            cnt++;
+
+            if(x == 0 && y == 0)
+                break;
+        }
+
+        System.out.println(cnt);
+    }
+}
