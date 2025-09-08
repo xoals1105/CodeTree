feabcf4ae6f2979b6c94cdf43b31968faf19c47f
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-08 19:04:01 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 274ms, Memory: 17MB, Status: Passed - Codetree

--- 250908/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.java
+++ 250908/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
+
+    public static int[] dx = new int[] {0, 1, 0, -1};
+    public static int[] dy = new int[] {1, 0, -1, 0};
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -9,9 +13,51 @@ public class Main {
         int[][] board = new int[n][n];
         for (int i = 0; i < n; i++) 
         {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < n; j++) 
+            {
                 board[i][j] = sc.nextInt();
             }
         }
+
+
+        int x = n / 2;
+        int y = n / 2;
+        int direction = 3;
+        int sum = board[x][y];
+
+
+            for(int i = 0; i < commands.length(); i++)
+            {
+    
+                switch(commands.charAt(i))
+                {
+                    case 'L':
+                        direction = (direction + 3) % 4;
+                        break;
+                    case 'R':
+                        direction = (direction + 1) % 4;
+                        break;
+                    case 'F':
+                        int newX = x + dx[direction];
+                        int newY = y + dy[direction];
+
+                        if (IsRange(newX, newY, n)) 
+                        {
+                       
+                            x = newX;
+                            y = newY;
+                        
+                            sum += board[x][y];
+                        }
+                        break;
+                }
+            }
+
+        System.out.println(sum);
+    }
+
+    private static boolean IsRange(int x, int y, int n)
+    {
+        return x >= 0 && x < n && y >= 0 && y < n;
     }
 }
