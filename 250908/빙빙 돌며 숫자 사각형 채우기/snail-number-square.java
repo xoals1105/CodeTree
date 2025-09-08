f2d3088fdd4340053c3ab58812a18ff2432ee692
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-08 13:18:29 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250908/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Main {
+
+    public static int[] dx = new int[] {0, 1, 0, -1};
+    public static int[] dy = new int[] {1, 0 ,-1, 0};
+
+
+    public static int n = 0;
+    public static int m = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        int x = 0;
+        int y = 0;
+        int newX = 0;
+        int newY = 0;
+        int direction = 0;
+        int[][] arr = new int[n][m];
+        arr[x][y] = 1;
+        for(int i = 2; i <= n * m; i++)
+        {
+            newX = x + dx[direction];
+            newY = y + dy[direction];
+            if(!IsRange(newX, newY) || arr[newX][newY] != 0)
+                direction = (direction + 1) % 4;
+            
+            x += dx[direction];
+            y += dy[direction];
+            arr[x][y] = i;     
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+                System.out.print(arr[i][j] + " ");
+            System.out.println();
+        }
+    }
+
+    private static boolean IsRange(int x, int y)
+    {
+        return x >= 0 && x < n && y >= 0 && y < m;
+    }
+}
