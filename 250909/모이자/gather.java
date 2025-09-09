ecb6d52488ff3c5aab6d81a35765e612b6113625
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-09 10:19:33 +0900

    Title: 모이자, Time: 136ms, Memory: 10MB, Status: Passed - Codetree

--- 250909/모이자/gather.java
+++ 250909/모이자/gather.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class Main {
 
+    public static final int INT_MAX = Integer.MAX_VALUE;
     public static final int MAX = 100;
     public static int[] house = new int[MAX];
 
@@ -11,7 +12,7 @@ public class Main {
         for (int i = 0; i < n; i++) 
             house[i] = sc.nextInt();
 
-        int answer = 100;
+        int answer = INT_MAX;
         for(int i = 0; i < n; i++)
         {
             int sum = 0;
