b119982580b977c397a4429b995e80f0f561df03
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-09 10:17:13 +0900

    Title: 모이자, Time: 138ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250909/모이자/gather.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+
+    public static final int MAX = 100;
+    public static int[] house = new int[MAX];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) 
+            house[i] = sc.nextInt();
+
+        int answer = 100;
+        for(int i = 0; i < n; i++)
+        {
+            int sum = 0;
+            for(int j = 0; j < n; j++)
+            {
+                sum += Math.abs(j - i) * house[j];
+            }
+            answer = Math.min(answer, sum);
+        }
+
+        System.out.println(answer);
+    }
+}
