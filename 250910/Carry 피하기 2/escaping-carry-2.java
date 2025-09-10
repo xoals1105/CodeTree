2513a55977dc81883bd005cdaa624ff90ec0ee1b
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-10 12:59:42 +0900

    Title: Carry 피하기 2, Time: 123ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250910/Carry 피하기 2/escaping-carry-2.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Main {
+
+    public static final int INT_MIN = Integer.MIN_VALUE;
+
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+    
+
+        int answer = INT_MIN;
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                for(int k = 0; k < n; k++)
+                {
+                    int sum = 0;
+                    if(NoCarry(arr[i], arr[j], arr[k]))
+                        sum = arr[i] + arr[j] + arr[k];
+
+                    answer = Math.max(answer, sum);
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    private static boolean NoCarry(int a, int b, int c)
+    {
+        while(a > 0 || b > 0 || c > 0)
+        {
+            int digitSum = (a % 10) + (b % 10) + (c % 10);
+            if(digitSum > 10) return false;
+            a /= 10;
+            b /= 10;
+            c /= 10;
+        }
+        return true;
+    }
+}
