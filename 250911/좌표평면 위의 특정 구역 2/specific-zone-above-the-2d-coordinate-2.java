0af078aa6e6f2cc48de21b279b91e5bbf758a8a6
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-11 16:14:45 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250911/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main {
+    public static final int INT_MAX = Integer.MAX_VALUE;
+    public static final int INT_MIN = Integer.MIN_VALUE;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] x = new int[N];
+        int[] y = new int[N];
+        for (int i = 0; i < N; i++) {
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
+        }
+
+        int answer = INT_MAX;
+        for(int i = 0; i < N; i++)
+        {
+            int maxX = INT_MIN;
+            int minX = INT_MAX;
+            int maxY = INT_MIN;
+            int minY = INT_MAX;
+            for(int j = 0; j < N; j++)
+            {
+                if(i == j)
+                    continue;
+                
+                maxX = Math.max(maxX, x[j]);
+                minX = Math.min(minX, x[j]);
+                maxY = Math.max(maxY, y[j]); 
+                minY = Math.min(minY, y[j]); 
+            }
+
+            int area = (maxX - minX) * (maxY - minY);
+            answer = Math.min(area, answer);
+        }
+
+        System.out.println(answer);
+    }
+}
