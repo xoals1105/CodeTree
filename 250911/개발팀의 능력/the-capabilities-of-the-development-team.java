322e0dc6d32672ec5c120795a976956fc02320c8
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-11 12:06:13 +0900

    Title: 개발팀의 능력, Time: 123ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250911/개발팀의 능력/the-capabilities-of-the-development-team.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Main {
+
+    public static final int INT_MAX = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+
+        int sum = 0;
+        for(int i = 0; i < 5; i++)
+        {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        int answer = INT_MAX;
+        for(int i = 0; i < 5; i++)
+            for(int j = i+1; j < 5; j++)
+                for(int k = 0; k < 5; k++)
+                    for(int q = k+1; q < 5; q++)
+                    {
+                        if(i == k || j == k || i == q || j == q)
+                            continue;
+                        answer = Math.min(answer, GetDiff(sum, i, j, k, q));
+                    }    
+        
+        if(answer == INT_MAX)
+            System.out.println("-1");
+        else
+            System.out.println(answer);   
+    }
+    
+    private static int GetDiff(int sum, int i, int j, int k, int q)
+    {
+        int teamA = i + j;
+        int teamB = k + q;
+        int teamC = sum - teamA - teamB;
+
+        if(teamA == teamB && teamB == teamC)
+            return INT_MAX;    
+        int diff = Math.abs(teamA - teamB);
+        diff = Math.max(diff, Math.abs(teamB - teamC));
+        diff = Math.max(diff, Math.abs(teamA - teamC));
+
+        return diff;
+    }
+}
