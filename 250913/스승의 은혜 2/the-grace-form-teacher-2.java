ae94674a1c40aec712b87f20e2bea5df31b706e9
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-13 20:40:16 +0900

    Title: 스승의 은혜 2, Time: 219ms, Memory: 13MB, Status: Passed - Codetree

--- 250913/스승의 은혜 2/the-grace-form-teacher-2.java
+++ 250913/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -1,37 +1,33 @@
-import java.util.Scanner;
-public class Main {
+import java.util.*;
 
-    public static final int INT_MIN = Integer.MIN_VALUE;
+public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int b = sc.nextInt();
+        int n = sc.nextInt();  // 학생 수
+        int b = sc.nextInt();  // 예산
         int[] p = new int[n];
+
         for (int i = 0; i < n; i++) {
             p[i] = sc.nextInt();
         }
 
+        int answer = 0;
 
-        int answer = INT_MIN;
-        for(int i = 0; i < n; i++)
-        {
-            int giftableStudent = 1;
-            int student = 0;
-            for(int j = 0; j < n; j++)
-            {
-                if(i == j)
-                { 
-                    student += p[i] / 2;
-                    continue;
-                }
-                student += p[j];
-                if(student > b)
-                    break;
-                giftableStudent++;
+        // i번째 학생을 할인 대상으로 설정
+        for (int i = 0; i < n; i++) {
+            int[] temp = p.clone();       // 배열 복사
+            temp[i] /= 2;                 // i번째 학생 가격 절반 할인
+            Arrays.sort(temp);            // 오름차순 정렬
+
+            int sum = 0, cnt = 0;
+            for (int j = 0; j < n; j++) {
+                if (sum + temp[j] > b) break;
+                sum += temp[j];
+                cnt++;
             }
 
-            answer = Math.max(answer, giftableStudent);
+            answer = Math.max(answer, cnt);
         }
 
         System.out.println(answer);
