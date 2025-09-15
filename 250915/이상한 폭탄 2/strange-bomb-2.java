17ce7e1a9e9c09abde0268ad696df484220e16f4
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-15 13:36:18 +0900

    Title: 이상한 폭탄 2, Time: 135ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250915/이상한 폭탄 2/strange-bomb-2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] bombs = new int[n];
+        for (int i = 0; i < n; i++) {
+            bombs[i] = sc.nextInt();
+        }
+
+        int answer = -1;
+        for(int i = 0; i < n - k; i++)
+        {
+            boolean find = false;
+            for(int j = 1; j <= k; j++)
+            {
+                // if(i == j) 
+                //     continue;
+
+                // for(int q = 1; q <= k; q++)
+                // {
+                //     if(bombs[])
+                // }
+
+                if(bombs[i] == bombs[i + j])
+                    find = true;
+            }
+
+            if(find)
+                answer = Math.max(answer, bombs[i]);
+        }
+        System.out.println(answer);
+    }
+}
