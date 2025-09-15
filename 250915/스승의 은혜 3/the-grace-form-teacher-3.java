1e41ce8602076ac40f67917e90386286cd438966
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-15 14:21:22 +0900

    Title: 스승의 은혜 3, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250915/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 250915/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int b = sc.nextInt();
         int[] p = new int[n];
-        int[] s = new int[n];
+        int[] s = new int[1001];
         for(int i = 0; i < n; i++){
             p[i] = sc.nextInt();
-            s[i] = sc.nextInt();
+            s[p[i]] = sc.nextInt();
         }
+
+        Arrays.sort(p);
 
         int answer = -1;
         for(int i = 0; i < n; i++)
@@ -19,9 +23,9 @@ public class Main {
             for(int j = 0; j < n; j++)
             {
                 if(i == j)
-                    gift += (p[i] / 2) + s[i];
+                    gift += (p[i] / 2) + s[p[i]];
                 else
-                    gift += p[j] + s[j];
+                    gift += p[j] + s[p[j]];
 
                 if(gift > b)
                     break;
