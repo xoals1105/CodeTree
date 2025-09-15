f71c9bce9e072b9c2db4e720c703ab9769e31c5f
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-15 14:38:13 +0900

    Title: 스승의 은혜 3, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

--- 250915/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 250915/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -1,38 +1,42 @@
-import java.util.Scanner;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int b = sc.nextInt();
+
         int[] p = new int[n];
-        int[] s = new int[1001];
-        for(int i = 0; i < n; i++){
+        int[] s = new int[n];
+
+        for (int i = 0; i < n; i++) {
             p[i] = sc.nextInt();
-            s[p[i]] = sc.nextInt();
+            s[i] = sc.nextInt();
         }
 
-        Arrays.sort(p);
+        int answer = 0;
 
-        int answer = -1;
-        for(int i = 0; i < n; i++)
-        {
-            int gift = 1;
-            int personCnt = 0;
-            for(int j = 0; j < n; j++)
-            {
-                if(i == j)
-                    gift += (p[i] / 2) + s[p[i]];
-                else
-                    gift += p[j] + s[p[j]];
-
-                if(gift > b)
-                    break;
-                personCnt++;
+        // i번째 학생에게 쿠폰을 적용한다고 가정
+        for (int i = 0; i < n; i++) {
+            int[] cost = new int[n];
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    cost[j] = (p[j] / 2) + s[j]; // 쿠폰 적용
+                } else {
+                    cost[j] = p[j] + s[j];
+                }
             }
 
-            answer = Math.max(answer, personCnt);
+            Arrays.sort(cost);
+
+            int sum = 0, cnt = 0;
+            for (int c : cost) {
+                if (sum + c > b) break;
+                sum += c;
+                cnt++;
+            }
+
+            answer = Math.max(answer, cnt);
         }
 
         System.out.println(answer);
