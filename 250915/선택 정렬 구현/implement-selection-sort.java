d63be83a35fede1e7b0e5f8cbcd2c61d85148430
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-15 16:40:34 +0900

    Title: 선택 정렬 구현, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250915/선택 정렬 구현/implement-selection-sort.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        selectSort(arr, n);
+
+        for(int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+
+    private static void selectSort(int[] arr, int n)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            int select = i;
+            for(int j = i + 1; j < n; j++)
+            {
+                if(arr[j] < arr[select])
+                    select = j;
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[select];
+            arr[select] = temp;
+        }
+    }
+}
