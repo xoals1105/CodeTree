a93f118b9a965d83fdfa309116ad0c7d112b37ef
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-16 14:44:24 +0900

    Title: 빙산의 일각 2, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250916/빙산의 일각 2/the-tip-of-the-iceberg-2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Main {
+
+    public static final int INT_MIN = Integer.MIN_VALUE;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] h = new int[n];
+        for (int i = 0; i < n; i++) {
+            h[i] = sc.nextInt();
+        }
+
+        int answer = INT_MIN;
+        for(int i = 1; i <= 1000; i++)
+        {
+            int[] iceberg = new int[n];
+            for(int j = 0; j < n; j++)
+            {
+                if(h[j] > i)
+                    iceberg[j] = 1;
+                else
+                    iceberg[j] = 0;
+            }
+
+            int cnt = 1;
+            for(int j = 0; j < n - 1; j++)
+            {
+                boolean find = false;
+                if(iceberg[j] == 0 && iceberg[j + 1] == 1)
+                    cnt++;
+            }
+
+            answer = Math.max(answer, cnt);
+        }
+
+        System.out.println(answer);
+    }
+}
