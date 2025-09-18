2e7dec5b91ca5485f1349539bafb3e30de3a9bc2
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-18 09:59:47 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 146ms, Memory: 10MB, Status: Passed - Codetree

--- 250918/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.java
+++ 250918/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.java
@@ -12,17 +12,17 @@ public class Main {
     private static int SpecificNumber(int A, int B, int C)
     {
         int answer = 0;
-        for(int i = 0; i <= 100; i++)
+        for(int i = 0; i <= 1000; i++)
         {
             int num = 0;
-            for(int j = 0; j <= 100; j++)
+            for(int j = 0; j <= 1000; j++)
                 if((A * i) + (B * j) <= C)
                     num = (A * i) + (B * j);
 
             answer = Math.max(answer, num);
 
             num = 0;
-             for(int j = 0; j <= 100; j++)
+             for(int j = 0; j <= 1000; j++)
                 if((A * j) + (B * i) <= C)
                     num = (A * j) + (B * i); 
 
