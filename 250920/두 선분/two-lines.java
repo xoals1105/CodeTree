01ab8cd1fc46a2c3151b3a1cff4c3218ca4bcfa2
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-20 16:41:26 +0900

    Title: 두 선분, Time: 128ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250920/두 선분/two-lines.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int x3 = sc.nextInt();
+        int x4 = sc.nextInt();
+
+        if(MeetSegment(x1, x2, x3, x4))
+            System.out.println("intersecting");
+        else
+            System.out.println("nonintersecting");
+    }
+
+    private static boolean MeetSegment(int x1, int x2, int x3, int x4)
+    {
+        if(x3 <= x1 && x4 >= x2)
+            return true;
+        if(x1 <= x3 && x4 <= x2)
+            return true;
+        if(x3 <= x2 || x1 <= x4)
+            return true;
+
+        return false;
+    }
+}
