5f61e1e10d182f737d64a1033a1fce558e4ccfbc
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-20 16:55:10 +0900

    Title: 두 선분, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

--- 250920/두 선분/two-lines.java
+++ 250920/두 선분/two-lines.java
@@ -15,8 +15,8 @@ public class Main {
 
     private static boolean MeetSegment(int x1, int x2, int x3, int x4)
     {
-        if(x2 < x3 || x4 < x1)
-            return false;
-        return true;
+        if (x1 <= x4 && x3 <= x2)
+            return true;
+        return false;
     }
 }
