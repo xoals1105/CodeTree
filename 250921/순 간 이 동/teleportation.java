88c3d955dadc29ecef3606492e6cedbbdb21a68a
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-21 18:58:45 +0900

    Title: 순 간 이 동, Time: 122ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250921/순 간 이 동/teleportation.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    
+    public static final int INT_MAX = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+    
+        int answer = INT_MAX;
+
+        int walkAX = Math.abs(A - x);
+        int walkAY = Math.abs(A - y);
+
+        int walkYB = Math.abs(B - y);
+        int walkXB = Math.abs(B - x);
+
+        int disFirst = walkAX + walkYB;
+        int disSecond = walkAY + walkXB;
+
+        answer = Math.min(disFirst, disSecond);
+
+        System.out.println(answer);
+    }
+}
