ca63dab549f4386a152331fac941c3b3bde3d96f
Mermin <72680084+xoals1105@users.noreply.github.com>
2025-09-23 14:04:38 +0900

    Title: 세 수의 최대 곱, Time: 3064ms, Memory: 34MB, Status: Time Limit Exceed - Codetree

--- 250923/세 수의 최대 곱/maximum-product-of-three-numbers.java
+++ 250923/세 수의 최대 곱/maximum-product-of-three-numbers.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Main {
+
+    public static final int INT_MIN = Integer.MIN_VALUE;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -9,7 +12,7 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
-        int answer = 0;
+        int answer = INT_MIN;
         for(int i = 0; i < n; i++)
             for(int j = i + 1; j < n; j++)
                 for(int k = j + 1; k < n; k++)
